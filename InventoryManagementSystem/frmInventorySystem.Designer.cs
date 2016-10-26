1784aa875b33004d93d33d81fbba2ec2ffe99fed
Momo Johnson <mo12g13@gmail.com>
2016-10-25 20:44:59 -0500

    First Commit on Inventory Management

---
+++ InventoryManagementSystem/frmInventorySystem.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace InventoryManagementSystem
+{
+    partial class frmInventorySystem
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBookTitle = new System.Windows.Forms.TextBox();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.txtPublisher = new System.Windows.Forms.TextBox();
+            this.btnAddInventory = new System.Windows.Forms.Button();
+            this.lstSummary = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Momo Johnson, Boris Valle, Carlos Melendez";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(13, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Book Title:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 87);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Author:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(19, 122);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(70, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Publisher: ";
+            // 
+            // txtBookTitle
+            // 
+            this.txtBookTitle.Location = new System.Drawing.Point(128, 52);
+            this.txtBookTitle.Name = "txtBookTitle";
+            this.txtBookTitle.Size = new System.Drawing.Size(110, 20);
+            this.txtBookTitle.TabIndex = 5;
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(128, 87);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(109, 20);
+            this.txtAuthor.TabIndex = 6;
+            // 
+            // txtPublisher
+            // 
+            this.txtPublisher.Location = new System.Drawing.Point(128, 121);
+            this.txtPublisher.Name = "txtPublisher";
+            this.txtPublisher.Size = new System.Drawing.Size(106, 20);
+            this.txtPublisher.TabIndex = 7;
+            // 
+            // btnAddInventory
+            // 
+            this.btnAddInventory.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddInventory.Location = new System.Drawing.Point(19, 167);
+            this.btnAddInventory.Name = "btnAddInventory";
+            this.btnAddInventory.Size = new System.Drawing.Size(125, 23);
+            this.btnAddInventory.TabIndex = 8;
+            this.btnAddInventory.Text = "Add Inventory";
+            this.btnAddInventory.UseVisualStyleBackColor = true;
+            this.btnAddInventory.Click += new System.EventHandler(this.btnAddInventory_Click);
+            // 
+            // lstSummary
+            // 
+            this.lstSummary.FormattingEnabled = true;
+            this.lstSummary.Location = new System.Drawing.Point(267, 38);
+            this.lstSummary.Name = "lstSummary";
+            this.lstSummary.Size = new System.Drawing.Size(232, 173);
+            this.lstSummary.TabIndex = 9;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(319, 243);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // frmInventorySystem
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(511, 285);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lstSummary);
+            this.Controls.Add(this.btnAddInventory);
+            this.Controls.Add(this.txtPublisher);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.txtBookTitle);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmInventorySystem";
+            this.Text = "Inventory Management System";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBookTitle;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.TextBox txtPublisher;
+        private System.Windows.Forms.Button btnAddInventory;
+        private System.Windows.Forms.ListBox lstSummary;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ InventoryManagementSystem/frmInventorySystem.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InventoryManagementSystem
+{
+    public partial class frmInventorySystem : Form
+
+    {
+      
+
+
+        public frmInventorySystem()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddInventory_Click(object sender, EventArgs e)
+        {
+            List<InventoryItems> items = new List<InventoryItems>();
+            items.Add(new InventoryItems(txtBookTitle.Text, txtAuthor.Text, txtPublisher.Text));
+            foreach (InventoryItems item in items)
+            {
+                lstSummary.Items.Add("Item successfully Added to Inventory");
+                lstSummary.Items.Add("");
+                lstSummary.Items.Add(label2.Text +" "+item.getbookTitle());
+                lstSummary.Items.Add(label3.Text +" "+item.GetAuthorName());
+                lstSummary.Items.Add(label4.Text +" "+item.getPubisher());
+                lstSummary.Items.Add("");
+                lstSummary.Items.Add("*****************************");
+               
+               
+            }
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+         
+           
+        }
+    }
+    class InventoryItems
+    {
+        private string bookTitle;
+        private string authorName;
+        private string publisher;
+        private List<InventoryItems> inventories = new List<InventoryItems>();
+
+        public InventoryItems(string bookTitle, string authorname, string publisher)
+        {
+            this.bookTitle = bookTitle;
+            this.authorName = authorname;
+            this.publisher = publisher;
+        }
+
+        public bool AddInventories(string bookTitle, string authorname, string publisher)
+        {
+            return false;
+            
+        }
+        public InventoryItems findCustomer(String name)
+        {
+            for (int i = 0; i < this.inventories.Count; i++)
+            {
+                InventoryItems customer = this.inventories[i];
+                if (customer.getbookTitle().Equals(name));
+                {
+                    return customer;
+                }
+            }
+            return null;
+        }
+
+        public String getbookTitle()
+        {
+            return this.bookTitle;
+        }
+        public String GetAuthorName()
+        {
+            return this.authorName;
+        }
+        public String getPubisher()
+        {
+            return this.publisher;
+        }
+
+        public override string ToString()
+        {
+            return bookTitle + " "  + authorName + " " + publisher;
+        }
+    }
+}
